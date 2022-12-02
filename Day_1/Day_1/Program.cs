07032c8b796b8e5f5a1582ebe1b71a6c6fe30fb2
Marimuthu <maroldo26@hotmail.com>
2022-12-02 11:17:05 +0530

    Solution for Day 1 & Day 2 puzzles

---
+++ Day_1/Day_1/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Puzzle_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Puzzle1();
+            Puzzle2();
+        }
+
+        private static void Puzzle1()
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            int Highestcalories = 0;
+            int currentCalories = 0;
+
+            foreach (var item in input)
+            {
+                if (string.IsNullOrEmpty(item))
+                {
+                    if(currentCalories > Highestcalories)
+                        Highestcalories= currentCalories;
+
+                    currentCalories = 0;
+                    continue;
+                }
+
+                if (int.TryParse(item, out int calorieUnit))
+                    currentCalories += calorieUnit;
+            }
+
+            Console.WriteLine(Highestcalories);
+        }
+
+        private static void Puzzle2()
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            int currentCalories = 0;
+            int[] top3Calories = new int[3];
+
+            foreach (var item in input)
+            {
+                if (string.IsNullOrEmpty(item))
+                {
+                    if (currentCalories > top3Calories[0])
+                    {
+                        top3Calories[2] = top3Calories[1];
+                        top3Calories[1] = top3Calories[0];
+                        top3Calories[0] = currentCalories;
+                    }
+                    else if (currentCalories > top3Calories[1])
+                    {
+                        top3Calories[2] = top3Calories[1];
+                        top3Calories[1] = currentCalories;
+                    }
+                    else if (currentCalories > top3Calories[0])
+                    {
+                        top3Calories[2] = currentCalories;
+                    }
+
+                    currentCalories = 0;
+                    continue;
+                }
+
+                if (int.TryParse(item, out int calorieUnit))
+                    currentCalories += calorieUnit;
+            }
+
+            Console.WriteLine(top3Calories.Sum());
+        }
+    }
+}
---
+++ Day_2/Day_2/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Puzzle_1();
+            Puzzle_2();
+        }
+
+        private static void Puzzle_1()
+        {
+            var input = File.ReadAllLines("Input.txt");
+
+            int totalScore = 0;
+
+            foreach (var item in input)
+            {
+                if (item.Length < 3)
+                    continue;
+
+                char opponetChoice = item[0];
+                char myChoice = item[2];
+
+                int opponentScore = 0, myScore = 0;
+
+                opponentScore = opponetChoice == 'A' ? 1 : opponetChoice == 'B' ? 2 : 3;
+                myScore = myChoice == 'X' ? 1 : myChoice == 'Y' ? 2 : 3;
+
+                int result = 0;
+
+                // Draw
+                // Rock vs Paper => Player 2 wins
+                // Rock vs Scissors => Player 1 wins
+                // Paper vs Rock=> Player 1 wins
+                // Paper vs Scissors=> Player 2 wins
+                // Scissors vs Rock=> Player 2 wins
+                // Scissors vs Paper=> Player 1 wins
+
+                if (opponentScore == myScore ) 
+                {
+                    result = 3;
+                }
+                else if((opponentScore == 1 && myScore == 2)|| (opponentScore == 2 && myScore == 3) ||
+                    (opponentScore == 3 && myScore == 1))// Rock vs Paper
+                {
+                    result= 6;
+                }
+
+                totalScore+= (result + myScore);
+            }
+
+            Console.WriteLine(totalScore);
+        }
+
+        private static void Puzzle_2()
+        {
+            var input = File.ReadAllLines("Input.txt");
+
+            int totalScore = 0;
+
+            foreach (var item in input)
+            {
+                if (item.Length < 3)
+                    continue;
+
+                char opponetChoice = item[0];
+                char myChoice = item[2];
+
+                int opponentScore = 0, myScore = 0;
+
+                opponentScore = opponetChoice == 'A' ? 1 : opponetChoice == 'B' ? 2 : 3;
+                
+                int result = myChoice == 'X' ? 0 : myChoice == 'Y' ? 3 : 6;
+
+
+                // 1 = Lose, 2 = Draw, 3 = Win
+
+                if (result == 3)
+                {
+                    myScore = opponentScore;
+                }
+                else if(result == 0) // Lose
+                {
+                    myScore = opponetChoice == 'A' ? 3 : opponetChoice == 'B' ? 1 : 2;
+                }
+                else if(result == 6) // Win
+                {
+                    myScore = opponetChoice == 'A' ? 2 : opponetChoice == 'B' ? 3 : 1;
+                }
+
+                totalScore += (result + myScore);
+            }
+
+            Console.WriteLine(totalScore);
+        }
+    }
+}
