78cce69239a2400bf3a44d4517abd1060eccb162
Marimuthu <maroldo26@gmail.com>
2022-12-06 02:00:10 +0530

    Day 3 & day 4 challnge updated

---
+++ Day_3/Day_3/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Reflection.Emit;
+
+namespace Day_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Puzzle2();
+        }
+
+        private static void Puzzle1()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+
+            int lineCount = lines.Count();
+            char[] sameItems = new char[lines.Count()];
+
+            for (int lineIndex  = 0; lineIndex < lineCount; lineIndex++)
+            {
+                string line = lines[lineIndex];
+                int compartmentLength = line.Length / 2;
+                char commonItem = '\0';
+
+                for (int i = 0; i < compartmentLength; i++)
+                {
+                    for (int j = compartmentLength; j < line.Length; j++)
+                    {
+                        if (line[i] == line[j])
+                        {
+                            commonItem = line[j];
+                            break;
+                        }
+                    }
+
+                    if (commonItem != '\0')
+                        break;
+                }
+
+                sameItems[lineIndex] = commonItem;
+            }
+
+            int total = 0;
+
+            foreach (var item in sameItems)
+            {
+                if(item >= 65 && item <= 90)
+                {
+                    total += item - 38;
+                }
+                else if(item >= 97 && item <= 122)
+                {
+                    total += item - 96;
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+
+        private static void Puzzle2()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+
+            int totalGroup = lines.Count() / 3;
+            char[] sameItems = new char[totalGroup];
+            int step = 0;
+
+            for (int i = 0; i < totalGroup; i++)
+            {
+                string ruckSack1 = lines[step++];
+                string ruckSack2 = lines[step++];
+                string ruckSack3 = lines[step++];
+
+                for (int sack1 = 0; sack1 < ruckSack1.Length; sack1++)
+                {
+                    for (int sack2 = 0; sack2 < ruckSack2.Length; sack2++)
+                    {
+                        if (ruckSack1[sack1] == ruckSack2[sack2])
+                        {
+                            for (int sack3 = 0; sack3 < ruckSack3.Length; sack3++)
+                            {
+                                if (ruckSack2[sack2] == ruckSack3[sack3])
+                                {
+                                    sameItems[i] = ruckSack1[sack1];
+                                    goto additem;
+                                }
+                            }
+                        }
+                    }
+
+                    additem:
+                    continue;
+                }
+            }
+
+            int total = 0;
+
+            foreach (var item in sameItems)
+            {
+                if (item >= 65 && item <= 90)
+                {
+                    total += item - 38;
+                }
+                else if (item >= 97 && item <= 122)
+                {
+                    total += item - 96;
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ Day_4/Day_4/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Day_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Puzzle2();
+        }
+
+        private static void Puzzle1()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+            int totalPairs = 0;
+
+            foreach (var line in lines)
+            {
+                var pairs = line.Split(',');
+
+                string[] pair1Values = pairs[0].Split('-');
+                string[] pair2Values = pairs[1].Split('-');
+
+                int pair1StartValue = int.Parse(pair1Values[0]);
+                int pair1EndValue = int.Parse(pair1Values[1]);
+
+                int pair2StartValue = int.Parse(pair2Values[0]);
+                int pair2EndValue = int.Parse(pair2Values[1]);
+
+                if((pair2StartValue >= pair1StartValue && pair2EndValue <= pair1EndValue) ||
+                    (pair1StartValue >= pair2StartValue && pair1EndValue <= pair2EndValue))
+                {
+                    totalPairs++;
+                }
+
+            }
+
+            Console.WriteLine(totalPairs);
+        }
+
+        private static void Puzzle2()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+            int totalPairs = 0;
+
+            foreach (var line in lines)
+            {
+                var pairs = line.Split(',');
+
+                string[] pair1Values = pairs[0].Split('-');
+                string[] pair2Values = pairs[1].Split('-');
+
+                int pair1StartValue = int.Parse(pair1Values[0]);
+                int pair1EndValue = int.Parse(pair1Values[1]);
+
+                int pair2StartValue = int.Parse(pair2Values[0]);
+                int pair2EndValue = int.Parse(pair2Values[1]);
+
+                if ((pair2StartValue >= pair1StartValue && pair2EndValue <= pair1EndValue) ||
+                    (pair1StartValue >= pair2StartValue && pair1EndValue <= pair2EndValue))
+                {
+                    totalPairs++;
+                }
+                else if ((pair2StartValue >= pair1StartValue && pair2StartValue <= pair1EndValue) || 
+                    (pair2EndValue >= pair1StartValue && pair2EndValue <= pair1EndValue))
+                {
+                    totalPairs++;
+                }
+                else
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+
+            Console.WriteLine(totalPairs);
+        }
+    }
+}
